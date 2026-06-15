16581986a1739f231b5e7de884e200d7aca471b8
flamingo_lindo <vitorantunes2003@gmail.com>
2026-06-15 10:36:47 -0300

    fix: added text and color to progress indicator

--- app/src/main/java/com/example/weartwitch/presentation/screens/StartUp.kt
+++ app/src/main/java/com/example/weartwitch/presentation/screens/StartUp.kt
@@ -1,11 +1,15 @@
 package com.example.weartwitch.presentation.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.wear.compose.material3.CircularProgressIndicator
+import androidx.wear.compose.material3.ProgressIndicatorDefaults
+import androidx.wear.compose.material3.Text
 
 @Composable
 fun StartUp() {
@@ -14,6 +18,15 @@ fun StartUp() {
         contentAlignment = Alignment.Center
     )
     {
-        CircularProgressIndicator()
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            CircularProgressIndicator(
+                colors = ProgressIndicatorDefaults.colors(
+                    indicatorColor = Color(0xFF6441A5),
+                )
+            )
+            Text(
+                text = "Loading channels...",
+            )
+        }
     }
 }
